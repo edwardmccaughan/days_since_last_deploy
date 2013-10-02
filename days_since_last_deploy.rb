a155c358f3013f31318e17f90ddf36dc5298983f
edward mccaughan <emc@betterplace.org>
2013-10-02 17:19:36 +0200

    nice-ify-ing

--- days_since_last_deploy.rb
+++ days_since_last_deploy.rb
@@ -4,14 +4,11 @@ require 'date'
 
 def days_since_last_deploy(repo_directory)
   git = Git.open(repo_directory)
-  t = Time.now
   git.fetch
-  puts "took #{Time.now - t}"
 
   most_recent_deploy = git.tags.select{|t| t.name.include?("production_deploy")}.last
 
   date = most_recent_deploy.name.gsub "production_deploy_", ""
-  puts date
   tag_date_format = "%Y_%m_%d-%H_%M"
   time_of_last_deploy = DateTime.strptime(date, tag_date_format)
 
