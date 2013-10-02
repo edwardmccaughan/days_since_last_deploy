e382a7453e82b9b3000e35cde20e951db52984be
edward mccaughan <emc@betterplace.org>
2013-10-02 16:34:24 +0200

    nice-ify-ing

--- days_since_last_deploy.rb
+++ days_since_last_deploy.rb
@@ -2,16 +2,16 @@ require 'rubygems'
 require 'git'
 require 'date'
 
-repo_directory = "/Developer/betterplace"
-tag_date_format = "%Y_%m_%d-%H_%M"
+def days_since_last_deploy(repo_directory)
+  git = Git.open(repo_directory)
+  most_recent_deploy = git.tags.select{|t| t.name.include?("production_deploy")}.last
 
+  date = most_recent_deploy.name.gsub "production_deploy_", ""
+  tag_date_format = "%Y_%m_%d-%H_%M"
+  time_of_last_deploy = DateTime.strptime(date, tag_date_format)
 
-git = Git.open(repo_directory)
-most_recent_deploy = git.tags.select{|t| t.name.include?("production_deploy")}.last
+  (DateTime.now.to_date - time_of_last_deploy.to_date).to_i
+end
 
-date = most_recent_deploy.name.gsub "production_deploy_", ""
-time_of_last_deploy = DateTime.strptime(date, tag_date_format)
-
-days_since_last_deploy = (DateTime.now.to_date - time_of_last_deploy.to_date).to_i
-
-puts "it has been #{days_since_last_deploy} days since the last production deploy"
+days = days_since_last_deploy("/Developer/betterplace")
+puts "it has been #{days} days since the last production deploy"
